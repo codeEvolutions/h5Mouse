9a1466c2091a42701d484131b290baec0f774026
wangpage <wangpage@foxmail.com>
2019-08-11 23:50:08 +0800

    Add and use eslint

--- app.js
+++ app.js
@@ -3,14 +3,13 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
-var qr_image = require('qr-image');
-var robot = require("robotjs");
-var GyroProcessor = require('./lib/gyro-processor')
-var TouchProcessor = require('./lib/touch-processor')
+var qrImage = require('qr-image');
+var GyroProcessor = require('./lib/gyro-processor');
+var TouchProcessor = require('./lib/touch-processor');
 
 global.$setting = {
-  send_time_gap: 50,
-}
+  send_time_gap: 50
+};
 
 var app = express();
 
@@ -26,14 +25,14 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/index', indexRouter);
 app.use('/', indexRouter);
-app.use('/qrcode/url', (req,res)=>{
-  var qrcode = qr_image.image(req.query.url);
-  res.type("png");
+app.use('/qrcode/url', (req, res) => {
+  var qrcode = qrImage.image(req.query.url);
+  res.type('png');
   qrcode.pipe(res);
 });
 
-// error handler  
-app.use(function(err, req, res, next) {
+// error handler
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -42,36 +41,33 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+require('express-ws')(app);
 var wss = [];
-var expressWs = require('express-ws')(app);
-
-app.ws('/mouse', (ws, req)=>{
-  var gyrpProcessor = GyroProcessor("relative");
+app.ws('/mouse', (ws, req) => {
+  var gyrpProcessor = GyroProcessor('relative');
   var touchProcessor = TouchProcessor();
-  ws.on('message', function(msg) {
+  ws.on('message', function (msg) {
     var event = JSON.parse(msg);
-    if(!event.type) return;
-    if(event.type === "gyro"){
+    if (!event.type) return;
+    if (event.type === 'gyro') {
       gyrpProcessor.process(event);
-    }
-    else if(event.type === "touch"){
+    } else if (event.type === 'touch') {
       touchProcessor.process(event);
     }
   });
-  ws.on('connection',data=>{
+  ws.on('connection', data => {
     wss.push(ws);
   });
-  ws.on("close",data=>{
-    console.log("close");
+  ws.on('close', data => {
+    console.log('close');
     var i = wss.indexOf(ws);
-    if(i > -1){
-      wss.splice(i,1);
+    if (i > -1) {
+      wss.splice(i, 1);
     }
-  })
-})
+  });
+});
 
-function onListening() {
-  debugger
+function onListening () {
   var addr = server.address();
   console.log('Listening on url: http://0.0.0.0:' + addr.port);
 }
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,21 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  if (!(typeof(req.query.app)!="undefine" && req.query.app === "test") && req.client.remoteAddress == "::ffff:127.0.0.1"){
+router.get('/', function(req, res, next) {
+  if (!(typeof req.query.app !== 'undefined' && req.query.app === 'test') && req.client.remoteAddress === '::ffff:127.0.0.1') {
     var interfaces = require('os').networkInterfaces();
     var urls = [];
-    for(var it in interfaces){
-      interfaces[it].forEach(i=>{
-        if(!i.internal && i.family === "IPv4"){
-          urls.push("http://" + i.address + ":" + req.headers.host.slice(req.headers.host.indexOf(":")+1));
+    for (var it in interfaces) {
+      interfaces[it].forEach(i => {
+        if (!i.internal && i.family === 'IPv4') {
+          urls.push('http://' + i.address + ':' + req.headers.host.slice(req.headers.host.indexOf(':') + 1));
         }
-      })
+      });
     }
-    res.render("index", { title: "Express-server", urls: urls});
-  }
-  else {
-    res.render("index_app", { title: "Express", ws_port: global.ws_port, time_gap: global.$setting.send_time_gap});
+    res.render('index', { title: 'Express-server', urls: urls });
+  } else {
+    res.render('index_app', { title: 'Express', ws_port: global.ws_port, time_gap: global.$setting.send_time_gap });
   }
 });
 
